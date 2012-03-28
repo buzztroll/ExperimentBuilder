8b196019914aefcec6a32290cc876d4d3322018b
BuzzTroll <buzztroll@gmail.com>
2012-03-27 14:11:44 -1000

    u

--- test.sh
+++ test.sh
@@ -1,12 +1,3 @@
 #!/bin/bash
 
-size=2048
-total=4
-for i in `seq 0 $total`
-do
-    time python node.py ./ $i $total $size &
-done
-
-wait
-
-time python merge.py ./ $size mand$size-$total.png
+python util.py
