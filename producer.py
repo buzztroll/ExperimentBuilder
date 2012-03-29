cb7d5172e81de7fbaed67b0f92632612532573a4
BuzzTroll <buzztroll@gmail.com>
2012-03-28 14:52:14 -1000

    k

--- producer.py
+++ producer.py
@@ -1,44 +1,61 @@
 import os
 import sys
 from kombu import BrokerConnection, Exchange, Queue, Producer
+from dashi import DashiConnection
 
 
-filename = "meta"
-fptr = open(filename, "r")
-amqpurl = fptr.readline().strip()
-exchange_name = fptr.readline().strip()
+def get_dashi_connection(self, name):
+    self.exchange = "default_dashi_exchange"
+    self.dashi = DashiConnection(name, self.amqpurl, self.exchange, ssl=False)
+    return self.dashi
 
-print exchange_name
-print amqpurl
-exchange = Exchange(exchange_name, type="direct")
-D_queue = Queue(exchange_name, exchange, routing_key=exchange_name, auto_delete=False, exclusive=False)
-connection = BrokerConnection(amqpurl)
-channel = connection.channel()
+def client_finished(rank=None):
+    pass
 
-queue = D_queue(channel)
-queue.declare()
-producer = Producer(channel, exchange, routing_key=exchange_name)
 
-total_workers = int(sys.argv[1])
-imgsize = int(sys.argv[2])
-name = sys.argv[3]
+def main():
+    filename = "meta"
+    fptr = open(filename, "r")
+    amqpurl = fptr.readline().strip()
+    exchange_name = fptr.readline().strip()
 
-s3url = ""
-if 'EC2_URL' in os.environ:
-    s3url = os.environ['EC2_URL']
-s3id = os.environ['EC2_ACCESS_KEY']
-s3pw = os.environ['EC2_SECRET_KEY']
+    print exchange_name
+    print amqpurl
+    exchange = Exchange(exchange_name, type="direct")
+    D_queue = Queue(exchange_name, exchange, routing_key=exchange_name, auto_delete=False, exclusive=False)
+    connection = BrokerConnection(amqpurl)
+    channel = connection.channel()
 
-for i in range(0, total_workers):
-    msg = {'program': 'python node.py %d %d %d' % (i, total_workers, imgsize),
-            'rank': i,
-            's3url': s3url,
-            's3id': s3id,
-            's3pw': s3pw,
-            'testname': name}
-    print "sending message %s" % (str(msg))
-    producer.publish(msg,
-                 exchange=exchange,
-                 routing_key=exchange_name,
-                 serializer="json")
+    queue = D_queue(channel)
+    queue.declare()
+    producer = Producer(channel, exchange, routing_key=exchange_name)
 
+    total_workers = int(sys.argv[1])
+    imgsize = int(sys.argv[2])
+    name = sys.argv[3]
+
+    s3url = ""
+    if 'EC2_URL' in os.environ:
+        s3url = os.environ['EC2_URL']
+    s3id = os.environ['EC2_ACCESS_KEY']
+    s3pw = os.environ['EC2_SECRET_KEY']
+
+
+
+    for i in range(0, total_workers):
+        msg = {'program': 'python node.py %d %d %d' % (i, total_workers, imgsize),
+                'rank': i,
+                's3url': s3url,
+                's3id': s3id,
+                's3pw': s3pw,
+                'testname': name}
+        print "sending message %s" % (str(msg))
+        producer.publish(msg,
+                     exchange=exchange,
+                     routing_key=exchange_name,
+                     serializer="json")
+
+
+if __name__ == "__main__":
+    rc = main()
+    sys.exit(rc)
