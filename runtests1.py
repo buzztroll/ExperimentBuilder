c73be3ea0da2bfa15bd153bcb79e56902eaa9400
BuzzTroll <buzztroll@gmail.com>
2012-03-30 14:36:52 -1000

    k

--- runtests1.py
+++ runtests1.py
@@ -2,19 +2,25 @@ from datetime import datetime
 import os
 import sys
 
-max_worker_count = 8
-max_picture_size = 1024 * 2
-picture_size = 1024
+max_worker_count = 128
+max_picture_size = 1024 * 16
 
 datafile = sys.argv[1]
 rnd= sys.argv[2]
 outf = open(datafile, "w")
 
-while picture_size < max_picture_size:
-    worker_count = 1
-    while worker_count <= max_worker_count:
+worker_count = max_worker_count
+while worker_count > 1:
+    
+    picture_size = 1024*8
+    while picture_size < max_picture_size:
 
         name = "exp%d_%d_%s" % (worker_count, picture_size, rnd)
+        name = name.lower()
+
+        cmd = "python listbucket.py %snimbus delete" % (name)
+        os.system(cmd)
+
         start_tm = datetime.now()
         cmd = "python producer.py %d %d %s" % (worker_count, picture_size, name)
         print cmd
@@ -23,7 +29,8 @@ while picture_size < max_picture_size:
 
         tm = end_tm - start_tm
         outf.write("%s %d %d %d\n" % (name, worker_count, picture_size, tm.total_seconds()))
+        outf.flush()
 
-        worker_count = worker_count + 1
+        worker_count = worker_count / 2
     
     picture_size = picture_size * 2
