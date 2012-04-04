d872bc1870a2714e6742134aedba98340097ea4f
BuzzTroll <buzztroll@gmail.com>
2012-04-03 15:21:13 -1000

    k

---
+++ chaos.py
@@ -0,0 +1,56 @@
+from datetime import datetime
+from boto.ec2.connection import EC2Connection
+import os
+import random
+import sys
+import urlparse
+import time
+
+def get_node_count(ec2conn, image_name="expr2.gz"):
+    instance_list = []
+    all_inst = ec2conn.get_all_instances()
+    for res in all_inst:
+        for i in res.instances:
+            if i.image_id == image_name and i.state == 'running':
+                instance_list.append(i)
+    return instance_list
+
+def get_boto_con():
+    id = os.environ['EC2_ACCESS_KEY']
+    pw = os.environ['EC2_SECRET_KEY']
+    cloud_url = os.environ['EC2_URL']
+    uparts = urlparse.urlparse(cloud_url)
+    is_secure = uparts.scheme == 'https'
+    ec2conn = EC2Connection(id, pw, host=uparts.hostname, port=uparts.port, is_secure=is_secure)
+    ec2conn.host = uparts.hostname
+
+    return ec2conn
+
+def kill_a_vm(boto_con):
+    instlist = get_node_count(boto_cons)
+    if not instlist:
+        return False
+    inst = random.choice(instlist)
+    inst.terminate()
+    n = datetime.now()
+    print "XXX killing %s %s" % (str(inst), str(n))
+    return True
+    
+
+def main(argv=sys.argv):
+    interval = int(argv[1])
+
+    boto_con = get_boto_con()
+
+    random.seed()
+
+    for i in range(0, int(to_kill_count)):
+        time.sleep(interval)
+        kill_a_vm(boto_con)
+
+
+    return 0
+
+if __name__ == "__main__":
+    rc = main()
+    sys.exit(rc)
