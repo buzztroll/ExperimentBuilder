298ba56d05385bc30b45d31aada8c61f017d4419
BuzzTroll <buzztroll@gmail.com>
2012-04-07 09:55:23 -1000

    k

--- run-em.sh
+++ run-em.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
-mkdir /tmp/exprdata
-logfile="/tmp/exprdata/overall.log"
+logdir="/usr/local/exprdata-`date +%s`"
+rm -rf $logdir
+mkdir $logdir
+logfile="$logdir/overall.log"
 touch $logfile
+
+echo $logdir
 
 unset CHAOS_KILL_COUNT
 
@@ -12,7 +16,7 @@ do
     python cleanup.py tee -a $logfile
 
     sleep 60
-    run_logfile="/tmp/exprdata/run$i.log"
+    run_logfile="$logdir/run$i.log"
 
     python runtest_new_domain.py data$i round$i | tee $run_logfile
 
