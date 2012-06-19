7bfb7249dbf515b19b87e7a942bc5f3dc424d7ad
BuzzTroll <buzztroll@gmail.com>
2012-06-19 01:17:10 -0500

    shorter checkpoint

--- runtest_new_domain.py
+++ runtest_new_domain.py
@@ -78,8 +78,8 @@ datafile = sys.argv[1]
 rnd= sys.argv[2].lower()
 outf = open(datafile, "w")
 
-worker_count = 4
-picture_size = 1024*32
+worker_count = 10
+picture_size = 1024*64
 
 name = "newexp%d_%d_%s" % (worker_count, picture_size, rnd)
 name = name.lower()
